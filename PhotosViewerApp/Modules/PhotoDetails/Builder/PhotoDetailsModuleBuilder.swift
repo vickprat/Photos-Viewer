c7ecf51b164ea034dd44dece8b2f68397f56d9a0
Prateek Khandelwal (pratvick) <pratvick@gmail.com>
2020-10-29 17:20:00 +0530

    Add PhotosDetailsModuleBuilder

---
+++ PhotosViewerApp/Modules/PhotoDetails/Builder/PhotoDetailsModuleBuilder.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDetailsModuleBuilder.swift
+//  PhotosViewerApp
+//
+//  Created by pratvick on 2020/10/29.
+//
+
+import Foundation
+
+final class PhotoDetailsModuleBuilder {
+
+    func build(with photo: Photo) -> PhotoDetailsViewController {
+        let detailsViewController = PhotoDetailsViewController()
+        let presenter = PhotoDetailsPresenter(photo: photo)
+        let router = PhotoDetailsRouter()
+
+        presenter.view = detailsViewController
+        presenter.router = router
+
+        detailsViewController.presenter = presenter
+        router.viewController = detailsViewController
+
+        return detailsViewController
+    }
+}
